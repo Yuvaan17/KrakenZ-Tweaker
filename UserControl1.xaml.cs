ec20ba02757d63b650f2ecf69798c3735a6aed54
Yuvaan17 <nevatiayuvaan@gmail.com>
2022-12-03 03:11:40 +0530

    Successfully fixed all problems

--- UserControl1.xaml.cs
+++ UserControl1.xaml.cs
@@ -45,14 +45,10 @@ namespace KrakenZ_Tweaker.UserControls
 
         }
 
-
-
-
-
         private void Restore_Point_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Creating a restore Point! Please wait for the confirmation message and don't close the app before it.");
-            ExecuteCommand("wmic.exe /Namespace:\\\\root\\default Path SystemRestore Call CreateRestorePoint \"<KrakenZ Tweaker Restore Point >\", 100, 7 ");
+            MessageBox.Show("Creating a restore Point! Please be patient wait for the confirmation message and don't close the app before it. This process will take a few minutes to complete.");
+            ExecuteCommand(@"wmic.exe /Namespace:\\root\default Path SystemRestore Call CreateRestorePoint ""KrakenZ Tweaker Restore Point"", 100, 7");
             MessageBox.Show("Restore Point was created Succesfully!");
 
         }
